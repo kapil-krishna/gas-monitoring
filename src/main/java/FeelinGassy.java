bc97eef4ecb2b9a3a83aee7a37344a859f05bdae
Kapil Krishna <kapil.krishna.yo@gmail.com>
2019-12-10 14:10:09 +0000

    added .gitignore

--- src/main/java/FeelinGassy.java
+++ src/main/java/FeelinGassy.java
@@ -7,11 +7,22 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.ResponseHeaderOverrides;
 import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClient;
+import com.amazonaws.services.sns.model.PublishRequest;
+import com.amazonaws.services.sns.util.Topics;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClient;
+import com.amazonaws.services.sqs.model.CreateQueueRequest;
+import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
+import org.apache.commons.codec.binary.Base64;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.List;
 
 public class FeelinGassy {
 
@@ -20,6 +31,21 @@ public class FeelinGassy {
         RetrieveData.getBucketData
                 (clientRegion,"eventprocessing-locationss3bucket-7mbrb9iiisk4", "locations.json");
 
+        AmazonSNS sns = AmazonSNSClient.builder().build();
+        AmazonSQS sqs = AmazonSQSClient.builder().build();
+
+        String myTopicArn = "arn:aws:sns:eu-west-2:099421490492:EventProcessing-snsTopicSensorDataPart1-QVDE0JIXZS1V";
+        String myQueueUrl = sqs.createQueue(new CreateQueueRequest("KapilsQueue")).getQueueUrl();
+
+        Topics.subscribeQueue(sns, sqs, myTopicArn, myQueueUrl);
+
+        sns.publish(new PublishRequest(myTopicArn, "Hello SNS World").withSubject("Subject"));
+
+        List<Message> messages = sqs.receiveMessage(new ReceiveMessageRequest(myQueueUrl)).getMessages();
+        if (messages.size() > 0) {
+            byte[] decodedBytes = Base64.decodeBase64((messages.get(0)).getBody().getBytes());
+            System.out.println("Message: " +  new String(decodedBytes));
+        }
     }
 }
 
