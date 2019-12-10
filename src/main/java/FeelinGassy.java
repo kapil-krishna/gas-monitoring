6164c3e455117aeb0ab50644d5d3c331d73249f2
Kapil Krishna <kapil.krishna.yo@gmail.com>
2019-12-10 13:14:22 +0000

    refactored method for fetching data into own class

--- src/main/java/FeelinGassy.java
+++ src/main/java/FeelinGassy.java
@@ -17,33 +17,9 @@ public class FeelinGassy {
 
     public static void main(String[] args) throws Exception {
         Regions clientRegion = Regions.EU_WEST_2;
-        String bucketName = "eventprocessing-locationss3bucket-7mbrb9iiisk4";
-        String key = "locations.json";
+        RetrieveData.getBucketData
+                (clientRegion,"eventprocessing-locationss3bucket-7mbrb9iiisk4", "locations.json");
 
-        S3Object headerOverrideObject = null;
-        try {
-            AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
-                    .withRegion(clientRegion)
-                    .build();
-
-            GetObjectRequest getObjectRequestHeaderOverride = new GetObjectRequest(bucketName, key);
-            headerOverrideObject = s3Client.getObject(getObjectRequestHeaderOverride);
-            displayTextInputStream(headerOverrideObject.getObjectContent());
-
-        } finally {
-            if (headerOverrideObject != null) {
-                headerOverrideObject.close();
-            }
-        }
-    }
-
-    private static void displayTextInputStream(InputStream input) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-        String line;
-        while ((line = reader.readLine()) != null) {
-            System.out.println(line);
-        }
-        System.out.println();
     }
 }
 
---
+++ src/main/java/RetrieveData.java
@@ -0,0 +1,41 @@
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class RetrieveData {
+
+    public static void getBucketData(Regions clientRegion, String bucketName, String fileName) throws Exception {
+
+        S3Object headerOverrideObject = null;
+        try {
+            AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                    .withRegion(clientRegion)
+                    .build();
+
+            GetObjectRequest getObjectRequestHeaderOverride = new GetObjectRequest(bucketName, fileName);
+            headerOverrideObject = s3Client.getObject(getObjectRequestHeaderOverride);
+            displayTextInputStream(headerOverrideObject.getObjectContent());
+
+        } finally {
+            if (headerOverrideObject != null) {
+                headerOverrideObject.close();
+            }
+        }
+    }
+
+    private static void displayTextInputStream(InputStream input) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
+        System.out.println();
+    }
+}
